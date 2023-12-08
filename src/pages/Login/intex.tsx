17caeba542732aec55c21177c86b1f011b29f6f4
Minji Choi <minj22222c@gmail.com>
2023-12-08 11:50:38 +0900

    로그인페이지 도메인주소 수정완료

--- src/pages/Login/intex.tsx
+++ src/pages/Login/intex.tsx
@@ -1,14 +1,22 @@
-import { MutableRefObject, useRef } from "react";
+import { MutableRefObject, useEffect, useRef, useState } from "react";
 import { StyledLogin } from "./styles";
 import { AiOutlineLock,AiOutlineUser } from "react-icons/ai";
+import { getDomain } from "@/utils/http";
 
 const Login = () => {
+  const [loginAddress, setLoginAddress] = useState("");
+
+  useEffect(()=> {
+    setLoginAddress(`${getDomain()}/api/auth/signin`);
+  }, [getDomain()]);
+  console.log(`로그인페이지-현재도메인:${loginAddress}`);
+  
 
   return (
     <StyledLogin>
     <div id="body">
       <div id="box">
-      <form action="http://localhost:8081/api/auth/signin" method="post">
+      <form action={loginAddress} method="post">
         <label><AiOutlineLock /><input type="text" name="publisherName" placeholder="출판사명"  /></label>
         <label><AiOutlineUser /><input type="password" name="password" placeholder="패스워드" /></label>
         <button>로그인</button>
